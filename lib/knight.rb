bdd0689d5fd175f61771609d76de316766710189
Mark Michaelis <mallenm1993@gmail.com>
2023-05-11 15:58:03 -0400

    Create project files and define initialize methods

---
+++ lib/knight.rb
@@ -0,0 +1,14 @@
+
+class Knight
+
+  def initialize(color, location)
+    @color = color
+    @location = location
+    @potential_moves = update_moves
+  end
+
+  def update_moves
+    #updates potential moves based on location and updates 
+  end
+  
+end
---
+++ lib/rook.rb
@@ -0,0 +1,13 @@
+
+class Rook 
+
+  def initialize(color, location)
+    @color = color
+    @location = location 
+    @potential_moves = update_moves
+  end
+
+  def update_moves
+    #updates potential moves based on location and updates 
+  end
+end
