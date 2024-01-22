afa67b5ca9f399dff3bd4e5598ce0e4df93f9acf
Mark Michaelis <mallenm1993@gmail.com>
2024-01-22 15:36:59 -0500

    Remove old code

--- main.rb
+++ main.rb
@@ -3,28 +3,3 @@ require_relative 'lib/game.rb'
 
 game = Game.new
 game.start_game
-
-#testing for in_check
-# game.update_all_moves
-# p game.in_check(game.white.king)
-
-
-#testing for execute move
-# game.display_board
-# p game.white.knight_one.location
-# p game.puts_in_check(game.white, game.white.knight_one, [0,2])
-# game.execute_move([1,0], [0,2])
-# p game.white.knight_one.location
-# game.display_board
-
-#testing for update_all_moves
-# game.white.pieces.each do |piece|
-#     puts piece.name 
-#     p piece.potential_moves
-#   end
-# game.update_all_moves
-# p "now show updated moves"
-# game.white.pieces.each do |piece|
-#     puts piece.name 
-#     p piece.potential_moves
-#   end
